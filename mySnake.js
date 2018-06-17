ccb03566c97dec15a438955d151872e0cb9af69b
Laubegel <lphilippe.aube@gmail.com>
2018-06-17 17:54:54 -0300

    added odd apple

--- mySnake.js
+++ mySnake.js
@@ -1,8 +1,14 @@
 var cvs = document.getElementById('canvas');
 var ctx = cvs.getContext('2d');
 
-var ii = 0;
 var grid = 16;
+var oddApple = {
+	x: grid * 4,
+	y: grid * 4,
+	state: 0,
+	ate: false,
+	have: false,
+};
 var snake = {
 	x: 160,
 	y: 160,
@@ -13,10 +19,12 @@ var snake = {
 };
 var count = 0;
 var score = 0;
+var highScore = 0;
 var apple = {
 	x: 320,
 	y: 320
 };
+var oddSquare = document.getElementById('square');
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
@@ -39,6 +47,11 @@ function loop() {
 
 	snake.x += snake.dx;
 	snake.y += snake.dy;
+	if (oddApple.have === true) {
+		oddSquare.style.visibility = 'visible';
+	} else {
+		oddSquare.style.visibility = 'hidden';
+	}
 
 	// Wrap snake's position on edge of screen
 	if (snake.x < 0) {
@@ -67,35 +80,64 @@ function loop() {
 	ctx.fillStyle = '#FF0000';
 	ctx.fillRect(apple.x, apple.y, grid-1, grid-1);
 
+	// Draw Odd apple
+	if (Math.floor(Date.now() / 1000) % 12 === 0 && oddApple.have === false) {
+		oddApple.state = 1;
+	}
+	if (oddApple.state === 1) {
+		ctx.fillStyle = '#5588FF';
+		ctx.fillRect(oddApple.x, oddApple.y, grid-1, grid-1);
+	}
+
 	// Draw snake
-	ctx.fillStyle = '#00FF00';
+	if (oddApple.ate === true) {
+		ctx.fillStyle = '#99FF99';
+	} else {
+	ctx.fillStyle = '#00FD00';
+	}
 	snake.cells.forEach(function(cell, index) {
 		ctx.fillRect(cell.x, cell.y, grid-1, grid-1);
+
 		
 		// Snake ate apple
 		if (cell.x === apple.x && cell.y === apple.y) {
 			snake.maxCells++;
 			score++;
+			if (score >= highScore) {
+				highScore = score;
+			}
 			apple.x = getRandomInt(0, 25) * grid;
 			apple.y = getRandomInt(0, 25) * grid;
 		}
+		// Snake ate odd apple
+		if (cell.x === oddApple.x && cell.y === oddApple.y) {
+			oddApple.state = 0;
+			oddApple.x = getRandomInt(0, 25) * grid;
+			oddApple.y = getRandomInt(0, 25) * grid;
+			oddApple.have = true;
+		}
+
 		// Check collision with self
-		for (var i = index + 1; i < snake.cells.length; i++) {
-			if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-				snake.x = 160;
-				snake.y = 160;
-				snake.cells = [];
-				snake.maxCells = 4;
-				snake.dx = grid;
-				snake.dy = 0;
-				apple.x = getRandomInt(0, 25) * grid;
-				apple.y = getRandomInt(0, 25) * grid;
-				score = 0;
+		if (oddApple.ate === false) {
+			for (var i = index + 1; i < snake.cells.length; i++) {
+				if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+					oddApple.have === false;
+					snake.x = 160;
+					snake.y = 160;
+					snake.cells = [];
+					snake.maxCells = 4;
+					snake.dx = grid;
+					snake.dy = 0;
+					apple.x = getRandomInt(0, 25) * grid;
+					apple.y = getRandomInt(0, 25) * grid;
+					score = 0;
+				}
 			}
 		}
-	document.getElementById("score").textContent = "Points: " + score;
+	document.getElementById('score').textContent = 'Apples: ' + score;
+	document.getElementById('highscore').textContent = 'High Score: ' + highScore;
 	});
-	ctx.fillStyle = "#00DD00";
+	ctx.fillStyle = '#00DD00';
 	ctx.fillRect(snake.cells[0].x, snake.cells[0].y, grid-1, grid-1);
 }
 
@@ -115,6 +157,16 @@ document.addEventListener('keydown', function(e) {
 	else if (e.key === 'ArrowDown' && snake.dy === 0) {
 		snake.dx = 0;
 		snake.dy = grid;
+	}
+	if (e.key === 'x' && oddApple.have === true) {
+		oddApple.have = false;
+		oddApple.ate = true;
+		setTimeout(function() {
+			oddApple.ate = false;
+		}, 3*1000);
+	}
+	if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
+	e.preventDefault();
 	}
 });
 requestAnimationFrame(loop);
